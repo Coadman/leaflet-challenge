57292cff938818338afadfdc6d0597b77760b9b8
Coadman <162220090+Coadman@users.noreply.github.com>
2024-08-17 10:27:26 -0500

    updating logic

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,12 @@
 // URL to the earthquake data (All Earthquakes in the Past 7 Days)
 const earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-
+var geojson;
 // Create the map object centered on the world with a starting zoom level
-var map = L.map('map').setView([20, 0], 2);
+var map = L.map("map", {center: [37.09, -95.71], zoom: 5});
 
 // Add the tile layer (background map) to the Leaflet map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Fetch the earthquake data
